c5e5c3470cfd820a53bf3376abed57f3138b08e6
MunishRawal <munishrawal0303@gmai.com>
2023-10-02 01:32:30 +0530

    feat: My first feature

---
+++ src/App.js
@@ -0,0 +1,154 @@
+
+
+function App() {
+  return (
+    <h3 class=" text-center text-[36px] md:text-[64px] lg:text-[72px] leading-[1] ">DBS CHARACTER STATS
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-[20px] md:gap-x-[30px] lg:gap-x-[50px] gap-y-[30px] lg:gap-y-[50px]
+     px-[15px] md:px-[40px] mt-[50px] 
+     font-space-grotesk text-[12px] xl:text-[14px]">
+      <div className="w-full relative bg-black bg-opacity-100 md:hover:bg-opacity-50 duration-200 px-[10px] md:px-[15px] xl:px-[20px] py-[10px] md:py-[15px] xl:py-[20px] rounded-[10px] text-[#f8f2f2]">
+        <div className=" relative aspect-square bg-black flex justify-center items-center ">
+          <img className=" w-full h-full object-cover"
+          src="./images/blackgoku.jpg" alt="category 1"></img>          
+        </div>
+        <div className="flex flex-col xl:flex-row justify-between items-start pt-[10px] w-full">
+          <div className="flex flex-col items-start basis-[55%] text-left">
+            <p><b>GOKU BLACK</b></p>
+            <p><i>2000hp</i></p>
+          </div>
+          <div className="flex flex-col items-start xl:item-end font-medium">
+            <p>100 <b>EH</b></p>
+            <p>10/1 KD</p>
+            <p></p>
+          </div>
+       </div>
+      </div>
+      <div className="w-full relative bg-black bg-opacity-100 md:hover:bg-opacity-50 duration-200 px-[10px] md:px-[15px] xl:px-[20px] py-[10px] md:py-[15px] xl:py-[20px] rounded-[10px] text-[#eeefea]">
+        <div className=" relative aspect-square bg-black flex justify-center items-center ">
+          <img className=" w-full h-full object-cover"
+          src="./images/vegeta.jpg" alt="category 1"></img>          
+        </div>
+        <div className="flex flex-col xl:flex-row justify-between items-start pt-[10px] w-full">
+          <div className="flex flex-col items-start basis-[55%] text-left">
+            <p><b>VEGETA</b></p>
+            <p><i>1500hp</i></p>
+          </div>
+          <div className="flex flex-col items-start xl:item-end font-medium">
+            <p>80 <b>EH</b></p>
+            <p>10/2 KD</p>
+            <p></p>
+          </div>
+       </div>
+      </div>
+      <div className="w-full relative bg-black bg-opacity-100 md:hover:bg-opacity-50 duration-200 px-[10px] md:px-[15px] xl:px-[20px] py-[10px] md:py-[15px] xl:py-[20px] rounded-[10px] text-[#eeefea]">
+        <div className=" relative aspect-square bg-black flex justify-center items-center ">
+          <img className=" w-full h-full object-cover"
+          src="./images/beerus.jpg" alt="category 1"></img>          
+        </div>
+        <div className="flex flex-col xl:flex-row justify-between items-start pt-[10px] w-full">
+          <div className="flex flex-col items-start basis-[55%] text-left">
+            <p><b>BEERUS</b></p>
+            <p><i>10000hp</i></p>
+          </div>
+          <div className="flex flex-col items-start xl:item-end font-medium">
+            <p>150 <b>EH</b></p>
+            <p>10/0 KD</p>
+            <p></p>
+          </div>
+       </div>
+      </div>
+      <div className="w-full relative bg-black bg-opacity-100 md:hover:bg-opacity-50 duration-200 px-[10px] md:px-[15px] xl:px-[20px] py-[10px] md:py-[15px] xl:py-[20px] rounded-[10px] text-[#eeefea]">
+        <div className=" relative aspect-square bg-black flex justify-center items-center ">
+          <img className=" w-full h-full object-cover"
+          src="./images/frieza.jpg" alt="category 1"></img>          
+        </div>
+        <div className="flex flex-col xl:flex-row justify-between items-start pt-[10px] w-full">
+          <div className="flex flex-col items-start basis-[55%] text-left">
+            <p><b>FRIEZA</b></p>
+            <p><i>1000hp</i></p>
+          </div>
+          <div className="flex flex-col items-start xl:item-end font-medium">
+            <p>60 <b>EH</b></p>
+            <p>10/4 KD</p>
+            <p></p>
+          </div>
+       </div>
+      </div>
+      <div className="w-full relative bg-black bg-opacity-100 md:hover:bg-opacity-50 duration-200 px-[10px] md:px-[15px] xl:px-[20px] py-[10px] md:py-[15px] xl:py-[20px] rounded-[10px] text-[#eeefea]">
+        <div className=" relative aspect-square bg-black flex justify-center items-center ">
+          <img className=" w-full h-full object-cover"
+          src="./images/kidbuu.jpg" alt="category 1"></img>          
+        </div>
+        <div className="flex flex-col xl:flex-row justify-between items-start pt-[10px] w-full">
+          <div className="flex flex-col items-start basis-[55%] text-left">
+            <p><b>KIDBUU</b></p>
+            <p><i>8000hp</i></p>
+          </div>
+          <div className="flex flex-col items-start xl:item-end font-medium">
+            <p>90 <b>EH</b></p>
+            <p>10/1 KD</p>
+            <p></p>
+          </div>
+       </div>
+      </div>
+      <div className="w-full relative bg-black bg-opacity-100 md:hover:bg-opacity-50 duration-200 px-[10px] md:px-[15px] xl:px-[20px] py-[10px] md:py-[15px] xl:py-[20px] rounded-[10px] text-[#eeefea]">
+        <div className=" relative aspect-square bg-black flex justify-center items-center ">
+          <img className=" w-full h-full object-cover"
+          src="./images/satan.jpg" alt="category 1"></img>          
+        </div>
+        <div className="flex flex-col xl:flex-row justify-between items-start pt-[10px] w-full">
+          <div className="flex flex-col items-start basis-[55%] text-left">
+            <p><b>SATAN</b></p>
+            <p><i>100hp</i></p>
+          </div>
+          <div className="flex flex-col items-start xl:item-end font-medium">
+            <p>5 <b>EH</b></p>
+            <p>0/10 KD</p>
+            <p></p>
+          </div>
+       </div>
+      </div>
+      <div className="w-full relative bg-black bg-opacity-100 md:hover:bg-opacity-50 duration-200 px-[10px] md:px-[15px] xl:px-[20px] py-[10px] md:py-[15px] xl:py-[20px] rounded-[10px] text-[#eeefea]">
+        <div className=" relative aspect-square bg-black flex justify-center items-center ">
+          <img className=" w-full h-full object-cover"
+          src="./images/whis.jpg" alt="category 1"></img>          
+        </div>
+        <div className="flex flex-col xl:flex-row justify-between items-start pt-[10px] w-full">
+          <div className="flex flex-col items-start basis-[55%] text-left">
+            <p><b>WHIS</b></p>
+            <p><i>20000hp</i></p>
+          </div>
+          <div className="flex flex-col items-start xl:item-end font-medium">
+            <p>800 <b>EH</b></p>
+            <p>10/0 KD</p>
+            <p></p>
+          </div>
+       </div>
+      </div>
+      <div className="w-full relative bg-black bg-opacity-100 md:hover:bg-opacity-50 duration-200 px-[10px] md:px-[15px] xl:px-[20px] py-[10px] md:py-[15px] xl:py-[20px] rounded-[10px] text-[#eeefea]">
+        <div className=" relative aspect-square bg-black flex justify-center items-center ">
+          <img className=" w-full h-full object-cover"
+          src="./images/jiren.jpg" alt="category 1"></img>          
+        </div>
+        <div className="flex flex-col xl:flex-row justify-between items-start pt-[10px] w-full">
+          <div className="flex flex-col items-start basis-[55%] text-left">
+            <p><b>JIREN</b></p>
+            <p><i>2500hp</i></p>
+          </div>
+          <div className="flex flex-col items-start xl:item-end font-medium">
+            <p>150 <b>EH</b></p>
+            <p>10/1 KD</p>
+            <p></p>
+          </div>
+       </div>
+      </div>
+
+
+     
+    </div>
+    </h3>
+   
+  );
+}
+
+export default App;
